172fb39fa9047efc9416765052c00efe469d9383
Tima Baisalov <baisalov.t98@gmail.com>
2025-11-14 17:44:48 -0600

    new text add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from DevOps pipeline!");
+  res.end("This is new text. I want to check ci-cd pipeline");
 });
 
 server.listen(3000, () => {
